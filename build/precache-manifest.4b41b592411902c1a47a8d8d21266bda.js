2d5b0c1741bbeb9ac8ef22f83e5178a873f604e1
Abhinav Reddy <abhinavreddyboddu@gmail.com>
2022-12-01 11:07:39 +0530

    merged frontend and backend

---
+++ build/precache-manifest.4b41b592411902c1a47a8d8d21266bda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "47aa94c87bd214c1576d",
+    "url": "/static/js/main.2bb7133d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3552e5e62bbf3d9f066e",
+    "url": "/static/js/2.eed347ee.chunk.js"
+  },
+  {
+    "revision": "5a42b56938563ebe147b02049a0fe920",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const CheckWin = require('./Checker')
 
 const io = require("socket.io")(3001, {
     cors: {
-        origin: ['http://localhost:3000']
+        origin: ['http://localhost:9200']
     }
 })
+
+const express = require('express')
+const path = require('path');
+const app = express()
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '/build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/build/'));
+});
+
+//cors
+const cors = require('cors')
+
+app.use(cors())
 
 // var Clients = 0;
 var Clients = new Map();
 
 io.on("connection", socket => {
     socket.on("connect-to-room", (roomId) => {
-        const numberOfClients = Clients.get(roomId);
         console.log(roomId);
+        const numberOfClients = Clients.get(roomId);
         if(numberOfClients === undefined){
             console.log("Inside Client 1")
             socket.join(roomId);
@@ -37,4 +54,9 @@ io.on("connection", socket => {
             Clients.delete(roomId);
         });
     })
+})
+
+const PORT = 9200
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
