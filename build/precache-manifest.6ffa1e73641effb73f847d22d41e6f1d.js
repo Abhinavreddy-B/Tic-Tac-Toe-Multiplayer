0e0f8550e174e98980f7cfa3fde9bee3a992fbb8
Abhinav Reddy <abhinavreddyboddu@gmail.com>
2022-12-02 13:41:59 +0530

    Deploying

--- build/precache-manifest.6ffa1e73641effb73f847d22d41e6f1d.js
+++ build/precache-manifest.6ffa1e73641effb73f847d22d41e6f1d.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "4e5d1398c10a5a901c78",
-    "url": "/static/js/main.0fee07d4.chunk.js"
+    "revision": "51a718680f98acd35bdc",
+    "url": "/static/js/main.f78c9c7f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.652e98d8.chunk.js"
   },
   {
-    "revision": "02043a38566a4f7ada207f8a3b2da5b9",
+    "revision": "ca6b5dcb37637ed766a8abb3ee5ee054",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.249582a97a288c51c4ff02b1fd5f4d03.js"
+  "/precache-manifest.6ffa1e73641effb73f847d22d41e6f1d.js"
 );
 
 workbox.clientsClaim();
